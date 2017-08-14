0e7a066b99c7ac5e791c009f6d795d3bb3312b3e
JuMaD <julian.dlugosch@gmail.com>
2017-08-14 18:46:21 +0200

    Updated to perform cycle measurements and start queue on Btn_click (adjusted GUI accordingly)

--- Testfile.py
+++ Testfile.py
@@ -1,34 +1,54 @@
 """Testing GUI setup to make a list of available Instruments and Select one. VISA Manger --> QtListWidget --> ManagedWindow (ListItem)"""
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-
+import visa
 import sys
 
-class myListWidget(QListWidget):
+class InstrumentList(QListWidget):
+    #initialize List and populate with visa instruments
+    def __init__(self):
+        super().__init__()
+
+        rm = visa.ResourceManager()
+        instrs = rm.list_resources()
+        for n, instr in enumerate(instrs):
+            # trying to catch errors in comunication
+            try:
+                res = rm.open_resource(instr)
+                # try to avoid errors from *idn?
+                try:
+                    # noinspection PyUnresolvedReferences
+                    idn = res.ask('*idn?')[:-1]
+                except visa.Error:
+                    idn = "Not known"
+                finally:
+                    res.close()
+                    self.addItem(str(n)+":"+str(instr)+":"+str(idn))
+            except visa.VisaIOError as e:
+                print(n, ":", instr, ":", "Visa IO Error: check connections")
+                print(e)
+
+
+        rm.close()
+        #Resize width and height
+        self.resize(300,120)
+        self.setWindowTitle('Select Instrument')
+        #connect click handle
+        self.itemClicked.connect(self.Clicked)
 
     def Clicked(self,item):
-        QMessageBox.information(self, "ListWidget", "You clicked: "+item.text())
+        #call resource messenger
+        rm = visa.ResourceManager()
+        instrs = rm.list_resources()
 
-    def SelectInstrument():
 
-        listWidget = myListWidget()
+        QMessageBox.information(self, "Instrument Selection", "You clicked: "+item.text())
 
-        #Resize width and height
-        listWidget.resize(300,120)
 
-        listWidget.addItem("Item 1")
-        listWidget.addItem("Item 2")
-        listWidget.addItem("Item 3")
-        listWidget.addItem("Item 4")
-
-        listWidget.setWindowTitle('Select Instrument')
-        listWidget.itemClicked.connect(listWidget.Clicked)
-
-        listWidget.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ListOfInstruments = myListWidget()
-    ListOfInstruments.SelectInstrument()
+    ListOfInstruments = InstrumentList()
+    ListOfInstruments.show()
     sys.exit(app.exec_())
