719285ba6446b04da7ba311f3660994ae6af125a
JuMaD <julian.dlugosch@gmail.com>
2017-08-25 16:37:05 +0200

    Added Random Procedure from pymeasure doc for testing purposes

--- Main.py
+++ Main.py
@@ -1,41 +1,43 @@
-# Import necessary packages
-from pymeasure.instruments.keithley import Keithley2400
-from pymeasure.experiment import Procedure
-from pymeasure.experiment import IntegerParameter, FloatParameter
-from time import sleep
+#  Import necessary packages
+import re
 import sys
 import tempfile
-import numpy as np
 import uuid
-import re
+from time import sleep
 
+import numpy as np
 import visa
+from PyQt5.QtWidgets import *
 
-from pymeasure.log import console_log
 from pymeasure.display.Qt import QtGui
 from pymeasure.display.windows import ManagedWindow
-from pymeasure.experiment import Procedure, Results
 from pymeasure.experiment import IntegerParameter, FloatParameter, Parameter
+from pymeasure.experiment import Procedure, Results
+from pymeasure.instruments.keithley import Keithley2400
 
-from PyQt5.QtWidgets import *
-import visa
-import sys
 
-import multiprocessing
+# TODO: make a dictionnary that stores instrument_adress:instrument_class pairs as attribute of Procedure and define __init__(instruments={}).
+# That way, instruments become attributes of procedures and can _genrally_ be changed by other functions calling procedure.instruments[key]=value
 
+
+##########################
+# Measurement Procedures #
+##########################
 class IVCycles(Procedure):
     instrument_adress = "GPIB::4"
+
+    # define paramters here
     averages = IntegerParameter('Averages', default=50)
     max_voltage = FloatParameter('Maximum Voltage', units='V', default=1)
     min_voltage = FloatParameter('Minimum Voltage', units='V', default=-1)
     compliance = FloatParameter('Compliance', units='A', default=0.1)
-
-    cycles = IntegerParameter('#Cycles', default=1)
+    cycles = IntegerParameter('# Cycles', default=1)
     voltage_step = FloatParameter('Voltage Step', units='V', default=0.1)
-    #Calculate the number of data points from range and step
+    # Calculate the number of data points from range and step
     data_points = IntegerParameter('Data points',
                                    default=np.ceil((max_voltage.value - min_voltage.value) / voltage_step.value))
 
+    # define DATA_COLUMNS that are written to the file
     DATA_COLUMNS = ['Voltage (V)', 'Current (A)', 'Current Std (A)', 'Cycle']
 
 
@@ -45,17 +47,16 @@ class IVCycles(Procedure):
         log.info("Instrument Adress" + self.instrument_adress)
         self.sourcemeter = Keithley2400(self.instrument_adress)
         self.sourcemeter.reset()
-        #setting source mode to voltage, defining range and compliance
+        # setting source mode to voltage, defining range and compliance
         self.sourcemeter.apply_voltage([-20,20],self.compliance)
         self.sourcemeter.use_front_terminals()
         self.sourcemeter.measure_current()
-        sleep(0.1) # wait here to give the instrument time to react
+        sleep(0.1)  # wait here to give the instrument time to react
         self.sourcemeter.buffer_points = averages
 
-        #instruments = {}
-        #instruments[instrument_adress] =self.sourcemeter
-        print(instruments)
-
+        # instruments = {}
+        # instruments[instrument_adress] =self.sourcemeter
+        # print(instruments)
 
     def execute(self):
 
@@ -64,7 +65,7 @@ class IVCycles(Procedure):
             self.max_voltage.value,
             num=self.data_points.value
         )
-        #Loop through cycles
+        # Loop through cycles
         for cycle in xrange(cycles.value):
             # Loop through each voltage point, measure and record the current
             for voltage in voltages:
@@ -91,11 +92,9 @@ class IVCycles(Procedure):
         self.sourcemeter.shutdown()
         log.info("Finished measuring")
 
-#ATTENTION: TO RUN DIFFERENT PROCEDURES, DIFFERENT MAINWINDOW CLASSES HAVE TO BE DEFINED
-
 class Retention(Procedure):
-    #retention here
-    #paramet
+    # retention here
+    # paramet
 
     instrument_adress = "GPIB::4"
     averages = IntegerParameter('Averages', default=50)
@@ -109,7 +108,7 @@ class Retention(Procedure):
 
     def execute(self):
 
-        #SET Operation - one time only
+        # SET Operation - one time only
         log.info("Setting the voltage to %g V" % set_voltage)
         self.sourcemeter.source_voltage = voltage
         self.sourcemeter.reset_buffer()
@@ -129,18 +128,18 @@ class Retention(Procedure):
             log.info("User aborted the procedure")
         endtime = time.perf_counter()
 
-         #delay first measurement until read_delay time is reached
+        # delay first measurement until read_delay time is reached
         while endtime-starttime < read_delay:
             endtime = time.perf_counter()
 
-        #READ Operation in cycles
+        # READ Operation in cycles
 
         for cycle in xrange(cycles.value):
 
 
             self.sourcemeter.reset_buffer()
             sleep(0.1)
-            #start timer right before buffer and operations
+            # start timer right before buffer and operations
             starttime = time.perf_counter()
             self.sourcemeter.start_buffer()
             log.info("Waiting for the buffer to fill with measurements")
@@ -156,7 +155,7 @@ class Retention(Procedure):
                 log.info("User aborted the procedure")
                 break
             endtime = time.perf_counter()
-            #stay in this loop until read_delay is reached
+            # stay in this loop until read_delay is reached
             while endtime-starttime < self.read_delay:
                     endtime = time.perf_counter()
 
@@ -166,14 +165,46 @@ class Retention(Procedure):
 
 class Endurance(Procedure):
     print('Placeholder')
-    #define endurance measurements here
+    # define endurance measurements here
+
 
 class SwitchingEnergy(Procedure):
     print('Placeholder')
-    #define switching energy measurements here
+    # define switching energy measurements here
+
+
+class RandomProcedure(Procedure):
+    iterations = IntegerParameter('Loop Iterations')
+    delay = FloatParameter('Delay Time', units='s', default=0.2)
+    seed = Parameter('Random Seed', default='12345')
+
+    DATA_COLUMNS = ['Iteration', 'Random Number']
+
+    def startup(self):
+        log.info("Setting the seed of the random number generator")
+        random.seed(self.seed)
+
+    def execute(self):
+        log.info("Starting the loop of %d iterations" % self.iterations)
+        for i in range(self.iterations):
+            data = {
+                'Iteration': i,
+                'Random Number': random.random()
+            }
+            self.emit('results', data)
+            log.debug("Emitting results: %s" % data)
+            sleep(self.delay)
+            if self.should_stop():
+                log.warning("Caught the stop flag in the procedure")
+                break
+
+
+############################
+# Graphical User Interface #
+############################
 
 class MainWindow(ManagedWindow):
-
+    # TODO: Change constructor to be able to call MainWindow(Procedure, inputs, displays, x_axis, y_axis)
     def __init__(self):
         super(MainWindow, self).__init__(
             procedure_class=IVCycles,
@@ -184,10 +215,10 @@ class MainWindow(ManagedWindow):
         )
         self.setWindowTitle('Mol_measure 0.0.1')
 
-    #override queue fuction of managed window that gets executed upon clicking on 'Queue'
+    # override queue fuction of managed window that gets executed upon clicking on 'Queue'
     def queue(self):
-        #create filename based on procedure name, position in queue and a unique identifier
 
+        # create filename based on procedure name, position in queue and a unique identifier
         procedure_string = re.search("'(?P<name>[^']+)'",
                                      repr(self.procedure_class)).group("name")
         main_str, basename = procedure_string.split('.')
@@ -195,45 +226,61 @@ class MainWindow(ManagedWindow):
         uidn = uuid.uuid4().clock_seq_hi_variant
         filename = f'{basename}-{queue_position}_{uidn}.csv'
 
-
-        #call make_procedure from ManagedWindow to construct a new instance of procedure_class
+        # construct a new instance of procedure and define datapoints
         procedure = self.make_procedure()
-
-        #calculate number of datapoints
         procedure.data_points = np.ceil((procedure.max_voltage - procedure.min_voltage) / procedure.voltage_step)
 
-
-
-
-        #construct a new instance of Results
+        # construct a new instance of Results
         results = Results(procedure, filename)
-        #call new_experiment to construct a new Experiment (=convinient container for results + procedure)
+
+        # construct new experiment (that contains the previously constructed procedure and results objects)
         experiment = self.new_experiment(results)
-        #if its the first experiment don't start it right away
+
+        # if its the first experiment don't start it right away
         if not self.manager.experiments.queue:
             self.manager._start_on_add = False
             self.manager._is_continuous = False
             self.manager.queue(experiment)
 
-            #set the gui accordingly
+            # set the gui accordingly
             self.abort_button.setEnabled(False)
             self.abort_button.setText("Start")
             self.abort_button.clicked.disconnect()
             self.abort_button.clicked.connect(self.resume)
             self.abort_button.setEnabled(True)
 
-        #add experiment to que
+        # add experiment to que
         else:
-
             self.manager.queue(experiment)
 
 
+class MainWindowRandom(ManagedWindow):
+    def __init__(self):
+        super(MainWindowRandom, self).__init__(
+            procedure_class=RandomProcedure,
+            inputs=['iterations', 'delay', 'seed'],
+            displays=['iterations', 'delay', 'seed'],
+            x_axis='Iteration',
+            y_axis='Random Number'
+        )
+        self.setWindowTitle('GUI Example')
+
+    def queue(self):
+        filename = tempfile.mktemp()
+
+        procedure = self.make_procedure()
+        results = Results(procedure, filename)
+        experiment = self.new_experiment(results)
+
+        self.manager.queue(experiment)
+
+
 class InstrumentPicker(QListWidget):
-    #initialize List and populate with visa instruments
+    # initialize List and populate with visa instruments
 
     def __init__(self):
         super().__init__()
-        #bring window to front
+        # bring window to front
         self.raise_()
 
         rm = visa.ResourceManager()
@@ -256,26 +303,31 @@ class InstrumentPicker(QListWidget):
                 print(e)
 
         rm.close()
-        #Resize width and height
+        # Resize width and height
         self.resize(300,120)
         self.setWindowTitle('Select Instrument')
-        #connect click handle
+        # connect click handle
         self.itemClicked.connect(self.Clicked)
 
 
     def Clicked(self,item):
-        #pick instrument here!
-        #split item.text() string into n,instr,idn again
+        # pick instrument here!
+        # split item.text() string into n,instr,idn again
         itemtext = item.text()
         n, instr, idn = itemtext.split('-')
         QMessageBox.information(self, "Instrument Selection", "You clicked: \nitem\t\t"+n+"\nadress:\t\t"+instr+"\nidn:\t\t"+idn)
 
 
+################
+# Main Program #
+################
+
+
 if __name__ == "__main__":
 
     app = QtGui.QApplication(sys.argv)
-    window = MainWindow()
+    window = MainWindowRandom()
     window.show()
-    #picker = InstrumentPicker()
-    #picker.show()
+    # picker = InstrumentPicker()
+    # picker.show()
     sys.exit(app.exec_())
